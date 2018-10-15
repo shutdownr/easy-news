0827b0cafa92d2dfb9bf8fc888497cd46096f32b
Tim Kreuzer <tim@INTRANET.GOLLER.SITE>
2018-10-15 15:49:44 +0200

    Initial commit

---
+++ EasyNews/ErrorManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace EasyNews
+{
+    public class ErrorManager
+    {
+        private static readonly Lazy<ErrorManager> LazyErrorManager = new Lazy<ErrorManager>(() => new ErrorManager());
+
+        public static ErrorManager Instance { get { return LazyErrorManager.Value; } }
+
+        private ErrorManager()
+        {
+
+        }
+
+        public MessageBoxResult DisplayError(string message)
+        {
+            Trace.WriteLine($"\n\tError {message} occured");
+            return MessageBox.Show($"{message}", "Error occured!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
